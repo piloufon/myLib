25d4f603f311f9601a456833e91d84b01592249d
piloufon <piloufonsansd@gmail.com>
2025-08-19 10:41:54 +0200

    Create DX12.h

---
+++ DX12/DX12.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../WindowManager/WindowManager.h"
+
+#include "Device.h"
+#include "Factory.h"
+#include "CommandQueue.h"
+#include "Fence.h"
+#include "DescriptorHeapManager.h"
+#include "SwapChain.h"
+#include "BufferManager.h"
+
+namespace WindowManager {class WindowManager; }
+
+namespace DX12 {
+    
+    class DX12 {
+    public:
+        bool Initialize(WindowManager::WindowManager window);
+        void Shutdown();
+
+        void Update();
+    private:
+        WindowManager::WindowManager mainWindow;
+
+        Factory                 dxFactory;
+        Device                  dxDevice;
+        CommandQueue            dxCommandQueue;
+        Fence                   dxFence;
+        DescriptorHeapManager   dxDescriptorHeapManager;
+        SwapChain               dxSwapChain;
+        BufferManager           dxBufferManager;
+    };
+
+}
