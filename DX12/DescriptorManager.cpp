81dfb626bc81d2e537a6b19bbb0c289437fff514
piloufon <piloufonsansd@gmail.com>
2025-08-21 15:15:30 +0200

    Add files via upload

--- DX12/DescriptorManager.cpp
+++ DX12/DescriptorManager.cpp
@@ -2,15 +2,23 @@
 
 namespace DX12 {
     bool DescriptorManager::InitializeDescriptorHeapManager(ComPtr<ID3D12Device10> device) {
-        if(!InitRTVs()){
-            LOG_ERROR(L"DescriptorHeapManager - InitializeDescriptorHeapManager", L"Failed to execute InitRTVs");
+        this->device = device;
+
+        if (!InitRTVs()) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - InitializeDescriptorHeapManager", L"Failed to initialize descriptor heaps");
             return false;
         }
-        
+        if (!InitDSVs()) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - InitializeDescriptorHeapManager", L"Failed to initialize descriptor heaps");
+            return false;
+        }
+
         return true;
-	}
+    }
     void DescriptorManager::Shutdown() {
         rtvDescHeap.Reset();
+        dsvDescHeap.Reset();
+        device.Reset();
     }
 
     bool DescriptorManager::InitRTVs() {
@@ -22,13 +30,37 @@ namespace DX12 {
         };
 
         if (FAILED(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&rtvDescHeap)))) [[unlikely]] {
-            LOG_WARNING(L"DescriptorHeapManager - InitRTVs", L"Failed to create RTV descriptor heap");
+            LOG_WARNING(L"DescriptorManager - InitRTVs", L"Failed to create RTV descriptor heap");
             return false;
         }
-       
+
         rtvHeapStart = rtvDescHeap->GetCPUDescriptorHandleForHeapStart();
         rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        for (UINT8 i = 0; i < maxRTV; i++) {
+            freeRTVSlots.push(i);
+        }
+
         return true;
+    }
+    UINT8 DescriptorManager::AllocateRTV() {
+        if (freeRTVSlots.empty()) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - AllocateRTV", L"No free RTV slots available");
+            return INVALID_DESCRIPTOR_INDEX;
+        }
+
+        UINT8 index = freeRTVSlots.front();
+        freeRTVSlots.pop();
+
+        return index;
+    }
+    void DescriptorManager::FreeRTV(UINT8 index) {
+        if (index >= maxRTV) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - FreeRTV", L"Invalid RTV index");
+            return;
+        }
+
+        freeRTVSlots.push(index);
     }
 
     bool DescriptorManager::InitDSVs() {
@@ -38,15 +70,78 @@ namespace DX12 {
             .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
             .NodeMask = singleDevice
         };
-
         if (FAILED(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&dsvDescHeap)))) [[unlikely]] {
-            LOG_WARNING(L"DescriptorHeapManager - InitDSVs", L"Failed to create DSV descriptor heap");
+            LOG_WARNING(L"DescriptorManager - InitDSVs", L"Failed to create DSV descriptor heap");
             return false;
         }
 
         dsvHeapStart = dsvDescHeap->GetCPUDescriptorHandleForHeapStart();
         dsvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+        for (UINT8 i = 0; i < maxDSV; i++) {
+            freeDSVSlots.push(i);
+        }
+
         return true;
+    }
+    UINT8 DescriptorManager::AllocateDSV() {
+        if (freeDSVSlots.empty()) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - AllocateDSV", L"No free DSV slots available");
+            return INVALID_DESCRIPTOR_INDEX;
+        }
+
+        UINT8 index = freeDSVSlots.front();
+        freeDSVSlots.pop();
+
+        return index;
+    }
+    void DescriptorManager::FreeDSV(UINT8 index) {
+        if (index >= maxDSV) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - FreeDSV", L"Invalid DSV index");
+            return;
+        }
+
+        freeDSVSlots.push(index);
+    }
+    
+    bool DescriptorManager::InitSamplers() {
+        D3D12_DESCRIPTOR_HEAP_DESC desc{
+          .Type = D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER,
+          .NumDescriptors = maxSampler,
+          .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+          .NodeMask = singleDevice
+        };
+
+        if (FAILED(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&samplerDescHeap)))) [[unlikely]] {
+            LOG_WARNING(L"DescriptorManager - InitSamplers", L"Failed to create sampler descriptor heap");
+            return false;
+        }
+
+        samplerHeapStart = samplerDescHeap->GetCPUDescriptorHandleForHeapStart();
+        samplerDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER);
+
+        for (UINT16 i = 0; i < maxSampler; i++) {
+            freeSamplerSlots.push(i);
+        }
+
+        return true;
+    }
+    UINT16 DescriptorManager::AllocateSampler() {
+        if (freeSamplerSlots.empty()) [[unlikely]] {
+            LOG_ERROR(L"DescriptorManager - AllocateDSV", L"No free DSV slots available");
+            return INVALID_DESCRIPTOR_INDEX;
+        }
+        return UINT16();
+    }
+    UINT16 DescriptorManager::CreateSampler(const D3D12_SAMPLER_DESC& desc) {
+        UINT16 index = AllocateSampler();
+        if (index == INVALID_DESCRIPTOR_SAMPLER_INDEX) {
+            return INVALID_DESCRIPTOR_SAMPLER_INDEX;
+        }
+
+        D3D12_CPU_DESCRIPTOR_HANDLE handle = GetSamplerHandle(index);
+        device->CreateSampler(&desc, handle);
+
+        return index;
     }
 }
 
--- DX12/DescriptorManager.h
+++ DX12/DescriptorManager.h
@@ -18,39 +18,65 @@
 //
 
 namespace DX12 {
-	class DescriptorManager {
-	public:
-		bool InitializeDescriptorHeapManager(ComPtr<ID3D12Device10> device);
-		void Shutdown();
+    constexpr UINT8 INVALID_DESCRIPTOR_INDEX = UINT8_MAX;
+    constexpr UINT16 INVALID_DESCRIPTOR_SAMPLER_INDEX = UINT16_MAX;
 
-		inline D3D12_CPU_DESCRIPTOR_HANDLE GetRTVHandle(UINT8 index) {
-			D3D12_CPU_DESCRIPTOR_HANDLE handle = rtvHeapStart;
-			handle.ptr += index * rtvDescriptorSize;
-			return handle;
-		}
-		inline D3D12_CPU_DESCRIPTOR_HANDLE GetDSVHandle(UINT8 index) {
-			D3D12_CPU_DESCRIPTOR_HANDLE handle = dsvHeapStart;
-			handle.ptr += index * dsvDescriptorSize;
-			return handle;
-		}
-	private:
-		static constexpr UINT singleDevice = 0;   // Read https://learn.microsoft.com/en-us/windows/win32/api/d3d12/ns-d3d12-d3d12_descriptor_heap_desc
-		static constexpr UINT8 maxRTV = 256;
-		static constexpr UINT8 maxDSV = 32;
+    class DescriptorManager {
+    public:
+        bool InitializeDescriptorHeapManager(ComPtr<ID3D12Device10> device);
+        void Shutdown();
 
-		ComPtr<ID3D12Device10> device;
+        // Allocation/Deallocation
+        [[nodiscard]] UINT8 AllocateRTV();
+        void FreeRTV(UINT8 index);
+        [[nodiscard]] UINT8 AllocateDSV();
+        void FreeDSV(UINT8 index);
 
-		bool InitRTVs();
-		UINT rtvDescriptorSize;
-		D3D12_CPU_DESCRIPTOR_HANDLE rtvHeapStart;
-		ComPtr<ID3D12DescriptorHeap> rtvDescHeap;
+        [[nodiscard]] UINT16 AllocateSampler();
+        UINT16 CreateSampler(const D3D12_SAMPLER_DESC&); // TODO : Create a way to add D3D12_STATIC_SAMPLER_DESC but in the sahder -> PSO (not now)
+        // Handle getters
+        inline D3D12_CPU_DESCRIPTOR_HANDLE GetRTVHandle(UINT8 index) {
+            D3D12_CPU_DESCRIPTOR_HANDLE handle = rtvHeapStart;
+            handle.ptr += index * rtvDescriptorSize;
+            return handle;
+        }
+        inline D3D12_CPU_DESCRIPTOR_HANDLE GetDSVHandle(UINT8 index) {
+            D3D12_CPU_DESCRIPTOR_HANDLE handle = dsvHeapStart;
+            handle.ptr += index * dsvDescriptorSize;
+            return handle;
+        }
+        inline D3D12_CPU_DESCRIPTOR_HANDLE GetSamplerHandle(UINT16 index) {
+            D3D12_CPU_DESCRIPTOR_HANDLE handle = samplerHeapStart;
+            handle.ptr += index * samplerDescriptorSize;
+            return handle;
+        }
 
-		bool InitDSVs();
-		ComPtr<ID3D12DescriptorHeap> dsvDescHeap;
-		UINT dsvDescriptorSize;
-		D3D12_CPU_DESCRIPTOR_HANDLE dsvHeapStart;
-		
-	};
+    private:
+        static constexpr UINT singleDevice = 0;
+        static constexpr UINT8 maxRTV = UINT8_MAX;
+        static constexpr UINT8 maxDSV = 32;
+        static constexpr UINT16 maxSampler = 512; // Max is 2048 -> hardware limit
+
+        ComPtr<ID3D12Device10> device;
+
+        bool InitRTVs();
+        UINT rtvDescriptorSize;
+        D3D12_CPU_DESCRIPTOR_HANDLE rtvHeapStart;
+        ComPtr<ID3D12DescriptorHeap> rtvDescHeap;
+        std::queue<UINT8> freeRTVSlots;
+
+        bool InitDSVs();
+        UINT dsvDescriptorSize;
+        D3D12_CPU_DESCRIPTOR_HANDLE dsvHeapStart;
+        ComPtr<ID3D12DescriptorHeap> dsvDescHeap;
+        std::queue<UINT8> freeDSVSlots;
+
+        bool InitSamplers();
+        ComPtr<ID3D12DescriptorHeap> samplerDescHeap;
+        UINT samplerDescriptorSize;
+        D3D12_CPU_DESCRIPTOR_HANDLE samplerHeapStart;        
+        std::queue<UINT16> freeSamplerSlots;
+    };
 }
 
 
